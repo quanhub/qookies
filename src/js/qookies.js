99bba1e1f3684ab63c81e1074753473c8743cfe5
quan <quan.borapas@gmail.com>
2016-05-15 11:30:23 +0200

    added basic project structure

---
+++ src/js/qookies.js
@@ -0,0 +1,13 @@
+(function($){
+
+  $.fn.qookies = function(options){
+
+    var settings = $.extend({
+      position: 'bottomRight'
+    }, options);
+
+    var mainContainer = $(this);
+
+  };
+
+})(jQuery);
